118fc3d61dd20ca012baa058d0f0504dad387c5b
Fronx <fronx@soundcloud.com>
2011-01-02 19:54:21 +0100

    store queries

--- app.rb
+++ app.rb
@@ -2,14 +2,29 @@ require 'rubygems'
 require 'sinatra'
 require 'gap_query'
 require 'haml'
+require 'sequel'
 
 set :haml, {:format => :html5 }
+
+DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://my.db')
+
+begin
+  DB.schema(:queries)
+rescue Sequel::Error
+  DB.create_table :queries do
+   primary_key :id
+   String :text
+   DateTime :created_at
+   index :created_at
+  end
+end
 
 get '/' do
   redirect '/the more we * the less we *'
 end
 
 post '/' do
+  DB[:queries].insert(:text => params[:q], :created_at => Time.now)
   redirect "/#{params[:q]}"
 end
 
